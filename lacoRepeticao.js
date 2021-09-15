10bf60934eeb9383bba02e7d36956d3d20a97599
Victor Alexandre <76669738+VictorAlexandre1986@users.noreply.github.com>
2021-09-15 14:45:35 -0300

    Add files via upload

--- lacoRepeticao.js
+++ lacoRepeticao.js
@@ -4,6 +4,30 @@ for (nota of notas){
     console.log(`Nota : ${nota}`)
 }
 
+
+//------------------------------------------
+
+notas.forEach((valor,indice) => {
+    console.log(valor,indice)
+});
+
+//-------------------------------------------
+
+let cont=0;
+while(cont<=10){
+    console.log(cont);
+    cont++;
+}
+
+//--------------------------------------------
+
+do{
+    console.log(cont);
+    cont++;
+}while(cont<=20)
+
+//--------------------------------------------
+
 //For in é usando para percorrer objetos
 const pessoa = {
     nome:'Victor',
