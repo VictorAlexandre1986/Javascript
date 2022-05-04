7fabd2bcb7cf0fd351db545e18dff77685f527c2
Victor Alexandre <76669738+VictorAlexandre1986@users.noreply.github.com>
2022-05-04 20:17:07 -0300

    Add files via upload

--- switch.js
+++ switch.js
@@ -1,28 +1,35 @@
-function getDiaSemanaTexto(diaSemana){
-    let diaSemanaTexto;
+const data = new Date();
+const dia = data.getDay();
 
-    switch(diaSemana){
-        case 0 :
-            diaSemanaTexto = ' Domingo';
-            return diaSemanaTexto;
-        case 1 :
-            diaSemanaTexto = 'Segunda';
-            return diaSemanaTexto;
-        case 2 :
-            diaSemanaTexto = 'Terça';
-            return diaSemanaTexto;
-        case 3 :
-            diaSemanaTexto = 'Quarta';
-            return diaSemanaTexto;
-        case 4 :
-            diaSemanaTexto = 'Quinta';
-            return diaSemanaTexto;
-        case 5 :
-            diaSemanaTexto = 'Sexta';
-            return diaSemanaTexto;
-        case 6 :
-            diaSemanaTexto = 'Sábado';
-            return diaSemanaTexto;
+
+function diaSemana(dia){
+    let diaSemana;
+    switch(dia){
+        case 0:
+            diaSemana ="Domingo";
+            break;
+        case 1:
+            diaSemana ="Segunda";
+            break;
+        case 2:
+            diaSemana ="Terça";
+            break;
+        case 3:
+            diaSemana ="Quarta";
+            break;
+        case 4:
+            diaSemana ="Quinta"
+            break;
+        case 5:
+            diaSemana ="Sexta";
+            break;
+        case 6:
+            diaSemana ="Sábado";
+            break;
+        default:
+            diaSemana ="Não existe";
     }
-
+    return diaSemana;
 }
+
+console.log(diaSemana(dia));
