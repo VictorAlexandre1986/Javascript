81a5888f4e3a95e8488875cec407e842d33e0c6a
Victor Alexandre <76669738+VictorAlexandre1986@users.noreply.github.com>
2023-04-18 14:25:49 -0300

    Add files via upload

--- data.js
+++ data.js
@@ -1,40 +1,28 @@
-const data = new Date();
-console.log(data.toString());
 
-const data2 = new Date(2021,3,20,15,14,27)
-console.log(data2.toString())
+const retorna_hora = (data) => {
 
-umaDia = 60 * 60 * 24 * 1000
+    if (!data){
+        data = new Date();
+    }
 
-console.log('Dia', data.getDate());
-console.log('Mes', data.getMonth());
-console.log('Ano', data.getFullYear());
-console.log('Hora', data.getHours());
-console.log('Minutos', data.getMinutes());
-console.log('Segundos', data.getSeconds());
-console.log('ms', data.getMilliseconds());
-console.log('Dia da semana', data.getDay());
-const timestamp = Date.now();
-const data3 = new Date(timestamp)
-console.log(data3)
-
-function zeroAEsquerda(num){
-    return num >= 10 ? num : `0${num}`
+    x = data.toLocaleTimeString('pt-BR');
+    return x
 }
 
-function formataData(data){
-    const dia = zeroAEsquerda(data.getDate());
-    const mes = zeroAEsquerda(data.getMonth());
-    const ano = zeroAEsquerda(data.getFullYear());
-    const hora = zeroAEsquerda(data.getHours());
-    const minuto = zeroAEsquerda(data.getMinutes());
-    const segundos = zeroAEsquerda(data.getSeconds());
+data_atual = new Date();
+console.log(retorna_hora(data_atual));
 
-    return `${dia}/${mes}/${ano} ${hora}:${minuto}:${segundos}`
+
+const data_formatada = (data) =>{
+    if(!data){
+        data = new Date();
+    }
+
+    return data.toLocaleDateString('pt-BR');
 }
 
-const data4 = new Date()
-const dataBrasil = formataData(data4)
-console.log(dataBrasil)
+data_atual = new Date();
+console.log(data_formatada(data_atual));
+
 
 
